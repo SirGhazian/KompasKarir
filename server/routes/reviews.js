698387690b2767bbd6b2caebb9fddaed311a4106
SirGhazian <ghazianunp99@gmail.com>
2026-05-25 21:22:07 +0700

    tambah route get my review

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authToken = require("../middleware/authToken");
-const { createReview, getReviews } = require("../controllers/reviewController");
+const { createReview, getReviews, getMyReview } = require("../controllers/reviewController");
 
 // GET /api/reviews ---> list semua ulasan (publik)
 router.get("/", getReviews);
+
+// GET /api/reviews/me ---> cek apakah user sudah pernah review
+router.get("/me", authToken, getMyReview);
 
 // POST /api/reviews ---> tulis ulasan baru (perlu token)
 router.post("/", authToken, createReview);
